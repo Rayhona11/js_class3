37dda5e45f69ce4714e0de69f018bb211ecfce79
Rayhona <rayhonamirzoyeva.ru>
2024-01-30 15:22:23 +0500

    ftfyky

---
+++ script.js
@@ -0,0 +1,41 @@
+function abc(r =0) {
+    // () = Argument operatori
+    return 2 * 3,14 * r;
+}
+
+let a = abc(5)
+let b = abc(12)
+let c = abc()
+console.log(a);
+console.log(b);
+console.log(c);
+//? Parametr Operatori
+// console.log('Salom Dunyo');
+
+// Funksiyalar turlari
+
+// Deklarativ
+
+test()
+
+function test(){
+    console.log('test');
+}
+
+test()
+
+ // EXPRESS
+// let g = function () {
+//     console.log("EXPRESS");
+//     console.log(this);
+// }
+// g()
+
+// ARROW
+let h = () => {
+    console.log('ARROW');
+}
+
+let k = text => text
+// 1. Agar kod bir qator bo`lsa {} aqvs kerak emas
+// 2. Agar kod bir qator bo`lsa return operatori kerak emas
